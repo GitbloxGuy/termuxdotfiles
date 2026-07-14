4488b4e0ba5a2c3c597dddc8c53b17c44ab4c0c7
GitbloxGuy <Timfasae2@gmail.com>
2026-07-14 11:09:46 -0500

    Auto-commit: Tue Jul 14 11:09:46 CDT 2026

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,6 @@ alias ff='fastfetch -c ~/.config/fastfetch/config.jsonc'
 alias ff1='fastfetch -c ~/.config/fastfetch/config1.jsonc'
 alias x11='termux-x11 :1 -xstartup "dbus-launch --exit-with-session i3"'
 alias clearr='cd && clear && ff1'
-alias gpush='cd .config && git add . && git commit -m "Auto-commit: $(date)" && git push'
+alias gpush='cd && cd .config && git add . && git commit -m "Auto-commit: $(date)" && git push'
 
 
