09a88b06d3616252112086cdd4dfc8c3d37c7146
GitbloxGuy <Timfasae2@gmail.com>
2026-07-15 23:03:40 -0500

    Auto-commit: Wed Jul 15 23:03:40 CDT 2026

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,33 +1,7 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.mouse = 'a'            -- Enable mouse support
--- vim.opt.ignorecase = true      -- Ignore case in search patterns
--- vim.opt.smartcase = true       -- ...unless the search contains capitals
-vim.opt.hlsearch = true		-- Clear search highlights after matching
--- vim.opt.wrap = false           -- Don't wrap lines
---
--- -- Tabs & Indentation
--- vim.opt.tabstop = 4            -- Number of spaces a tab counts for
--- vim.opt.shiftwidth = 4         -- Size of an indent
--- vim.opt.expandtab = true       -- Convert tabs to spaces
---
--- -- ========================================================================== --
--- -- 2. KEYMAPS (Shortcuts)
--- -- ========================================================================== --
--- -- Set spacebar as your leader key (the prefix for custom shortcuts)
 vim.g.mapleader = ' '
---
--- -- Example: Pressing 'space + pv' will open the built-in file explorer (Netrw)
--- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open File Explorer' })
---
--- -- Fast escape from Insert mode using 'jk'
--- vim.keymap.set('i', 'jk', '<Esc>')
-
-
 require("config.lazy")
 require("config.autocmds")
-
+require("config.options")
 vim.api.nvim_create_autocmd("VimEnter", {
 	  callback = function()
 		      vim.cmd("Neotree show")
@@ -46,7 +20,6 @@ vim.g.clipboard = {
   cache_enabled = 0,
 }	
 
-vim.opt.clipboard = 'unnamedplus'
 -- Change the folder name color (e.g., to a nice light blue)
 vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#d4af37", bold = true })
 
